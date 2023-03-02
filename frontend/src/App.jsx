e6865fb6e66b4f62a1a8aa6aaa86d95b6179ab2f
AmitMali <amit23289@gmail.com>
2023-03-02 10:25:55 +0530

    initial react with vite setup with tailwind css installed and configured

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <>
+      {/*<!-- Component: One column even layout --> */}
+      <section>
+        <div className="container px-6 m-auto">
+          <div className="grid grid-cols-4 gap-6 md:grid-cols-8 lg:grid-cols-12">
+            <div className="col-span-4 md:col-span-8 lg:col-span-12">
+              Column 1/1
+            </div>
+          </div>
+        </div>
+      </section>
+      {/*<!-- End One column even layout --> */}
+    </>
+  );
+}
+
+export default App;
